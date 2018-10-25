ddf67e95ee7c7c4d72307bf1c49762919564a32d
nguyenducdung <nguyenducdung1803@gmail.com>
2018-10-25 15:12:03 +0700

    Initial commit

---
+++ app/src/main/java/rikkeisoft/nguyenducdung/com/appb/MainActivity.java
@@ -0,0 +1,45 @@
+package rikkeisoft.nguyenducdung.com.appb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private MyBroadcastReceiver myBroadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter("rikkeisoft.nguyenducdung.com.CALL_SUB_B");
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        if (intentFilter != null) {
+            registerReceiver(myBroadcastReceiver, intentFilter);
+            Intent intent = new Intent(this, SubActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (myBroadcastReceiver != null) {
+            unregisterReceiver(myBroadcastReceiver);
+        }
+    }
+
+    class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            String text = bundle.getString("keyb");
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
